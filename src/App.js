8e5f43d8509f910c6ae58de59336998613c5a8a5
samuelkayode <34770521+samuelkayode@users.noreply.github.com>
2019-04-10 19:06:55 +0100

    initial commit in face detection app project

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
+import './App.css';
+
+const app = new Clarifai.App({
+  apiKey: 'e80301fd2e9743fe9576acb4a154a234'
+})
+
+
+const particlesOptions = {
+  Particles:{
+    number:{
+      value:30,
+        density:{
+          enable:true,
+          value_area:800
+        }
+      }
+    }
+  }
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box:{},
+    }
+
+  }
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height=Number(image.height);
+    return{
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol:width - (clarifaiFace.right_col * width),
+      bottowRow: height -(clarifaiFace.bottom_row * height)
+    }
+    }
+
+    displayFaceBox = (box) => {
+      this.setState({box: box});
+    }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+}
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+    .predict(
+      Clarifai.FACE_DETECT_MODEL,
+       this.state.input )
+       .then(response =>this.displayFaceBox(this.calculateFaceLocation(response)))
+       .catch(err => console.log(err));
+    }
+    
+  render() {
+    return (
+      <div className="App">
+      <Particles className='particles'
+                params={particlesOptions}
+                />
+                    
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+        onInputChange={this.onInputChange} 
+        onButtonSubmit={this.onButtonSubmit} 
+        /> 
+        <FaceRecognition imageUrl={this.state.imageUrl} />
+      </div>
+    );
+  }
+}
+
+export default App;
